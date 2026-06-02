ea1360f4661c334a4a545ada4825b97c02d75e80
encanTado-A <e232code@gmail.com>
2026-06-02 19:35:31 +0800

    rename demo-db.js to demo-db.cjs

--- demo-db.cjs
+++ demo-db.cjs
@@ -1,9 +1,9 @@
 // Cjs
 const Database = require('better-sqlite3');
 const fs = require('fs');
-const env = require('dotenv');
+// const env = require('dotenv');
 
-env.config();
+// env.config();
 
 // check db exist
 const post_path = 'F:\\devp\\cccashflow\\databases\\';
@@ -25,22 +25,21 @@ const demo_table_name = [
     `TransactionLedger`,
 ];
 
-const demoConnectSqliteDB = async () => {
+let db = null;
+const demoConnectSqliteDB = () => {
     console.log("call from demo-db.js");
-    let db = null;
     try {
         // const db_file_path = post_path+demo_filename[0];
         const db_file_path = process.env.DEMO_DATABASE_PATH;
-        console.log(db_file_path)
         if ( ! fs.existsSync( db_file_path ) ) {
             console.log('File does not exists');
-            process.exit()
+            process.exit(-1);
         }
         
         db = new Database(db_file_path, { verbose: console.log, fileMustExist: true});
-        if ( db && db.open ) {
-            console.log('db open');
-        }
+        // if ( db && db.open ) {
+        //     console.log('db open');
+        // }
         // const Database = require('better-sqlite3')(db_file_path, { verbose: console.log });
         
         // for (let i = 0; i < demo_filename.length; i++) {
@@ -49,7 +48,7 @@ const demoConnectSqliteDB = async () => {
     }
     catch (error) {
         console.error('Database connection failed:', error.message);
-        process.exit(1);
+        process.exit(-1);
     }
     finally {
         console.log(`finish demoConnectSqliteDB`);
@@ -57,4 +56,5 @@ const demoConnectSqliteDB = async () => {
     return db;
 };
 
+// export default demoConnectSqliteDB;
 module.exports = demoConnectSqliteDB;
