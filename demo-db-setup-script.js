d56558ee6ebfdd3ebcfbefcd7926bb56a2f0af65
encanTado-A <e232code@gmail.com>
2026-06-02 21:28:59 +0800

    fix: setup script for table: Currency, AccountType, MoneyCategory

--- demo-db-setup-script.js
+++ demo-db-setup-script.js
@@ -1,20 +1,13 @@
 // necessary library
-// import Database from 'better-sqlite3';
-// import fs from 'fs';
-// import csv from 'csv-parser';
-
-// Cjs
+const path = require('node:path');
 const Database = require('better-sqlite3');
 const fs = require('fs');
 const csv = require('csv-parser');
 
-const results = [];
 // ##################################################
 
-// const HOSTNAME = '127.0.0.1';
-// // const PORT = 8000;
-
 // // read a csv
+// const results = [];
 // fs.createReadStream('../databases/demo-accounts.csv')
 //     .pi`pe(csv())
 //     .on('data', (data) => results.push(data))
@@ -24,22 +17,16 @@ const results = [];
 
 // ##################################################
 
-// // check csv exist
-// if ( process.argv.length != 4) {
-//     console.log('not enough arguement');
-//     console.log(`USAGE: node <script> <database_filename> <csv_filename>`)
-//     process.exit()
-// }
-// console.log(`target table: ${process.argv[2]}, target location: ${process.argv[3]}`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // check db exist
-const post_path = 'F:\\devp\\cccashflow\\databases\\';
 const demo_filename = [
     `demo.db`,
-    `demo-account-type.csv`,
-    `demo-accounts.csv`,
     `demo-currency.csv`,
+    `demo-account-type.csv`,
     `demo-money-category.csv`,
+    `demo-accounts.csv`,
 ];
 
 const demo_table_name = [
@@ -52,138 +39,105 @@ const demo_table_name = [
     `TransactionLedger`,
 ];
 
-const db_file_path = post_path+demo_filename[0];
+const db_file_path = path.join(__dirname, "databases", demo_filename[0]);
 if (! fs.existsSync( db_file_path )) {
     console.log('File does not exists');
     process.exit()
 }
 
-const db = new Database(db_file_path, { verbose: console.log, fileMustExist: true});
+const db = new Database(db_file_path, { verbose: console.log });
 if ( db && db.open) {
     console.log('db open');
 }
 // const Database = require('better-sqlite3')(db_file_path, { verbose: console.log });
 
 for (let i = 0; i < demo_filename.length; i++) {
-    console.log(`${i}th filename:\t\t${ post_path+demo_filename[i] }`)
+    console.log(`${i}th filename:\t\t${ demo_filename[i] }`)
 };
+
+// init tables
+async function setupDatabase(db) {
+    // Open/Create the database file
+
+    const tableAccountType = db.prepare( "CREATE TABLE IF NOT EXISTS AccountType (id INTEGER PRIMARY KEY , description VARCHAR(16))" );
+    const tableCurrency = db.prepare( "CREATE TABLE IF NOT EXISTS Currency (id CHAR(3) NOT NULL PRIMARY KEY, description VARCHAR(64))" );
+    const tableMoneyCategory = db.prepare( "CREATE TABLE IF NOT EXISTS MoneyCategory (id INTEGER PRIMARY KEY, description VARCHAR(64))" );
+    const tableAccounts = db.prepare( `CREATE TABLE IF NOT EXISTS Accounts (
+                id INTEGER PRIMARY KEY,
+                name VARCHAR(64) NOT NULL,
+                account_type INT NOT NULL,
+                description TEXT,
+                opening_balance DECIMAL(15, 2) DEFAULT 0,
+                currency CHAR(3) NOT NULL, 
+                money_category INT NOT NULL, 
+                created_at DATE NOT NULL,
+
+                FOREIGN KEY (account_type) REFERENCES AccountType(id),
+                FOREIGN KEY (currency) REFERENCES Currency(id),
+                FOREIGN KEY (money_category) REFERENCES MoneyCategory(id)
+                )` );
+
+    await db.exec(tableAccountType);
+    await db.exec(tableCurrency);
+    await db.exec(tableMoneyCategory);
+    await db.exec(tableAccounts);
+};
+
+setupDatabase(db).then( () => {
+    console.log('db setup completed');
+    const stmt = db.prepare("SELECT sql FROM sqlite_schema WHERE type IN ('table', 'index') AND sql NOT NULL;");
+    stmt.exec(".schema");
+});
 
 // ##################################################
 // read csv data and insert into db
 
-// for table: Currency
-
-// let stmt = db.prepare(`INSERT INTO ${demo_table_name[0]} VALUES (?, ?)`);
-let stmt = db.prepare(`INSERT INTO ${demo_table_name[0]} (id, description) VALUES (@id, @description)`);
-
-const insertMany = db.transaction ( (rows) => {
-    for ( const row of rows ) stmt.run(row);
-});
-
-const dataBuffer = [];
-
-if (! fs.existsSync( post_path+demo_filename[3] )) {
-    console.log('csv File does not exists');
-    process.exit()
-}
-console.log(post_path+demo_filename[3])
-
-fs.createReadStream( post_path+demo_filename[3] )
-    .pipe(csv())
-    .on('data', (row) => {
-        // row is an object where keys are the record
-        if (row.id !== undefined && row.description !== undefined) {
-            dataBuffer.push({
-                id: row.id, 
-                description: row.description
-            });
-        }
-    })
-    .on('end', () => {
-        try {
-            console.log(dataBuffer);
-            insertMany(dataBuffer);
-            // console.log(`csv to db completed`)
-            console.log(`CSV file for ${demo_filename[3]} has successfully processed. Total rows: ${dataBuffer.length}`);
-        } catch (error) {
-            console.error('Database insertion failed:', error);
-        }
+// for table: Currency, AccountType, MoneyCategory
+for (let i = 0; i < 3; i++) {
+    // ready the sql
+    let stmt = db.prepare(`INSERT INTO ${demo_table_name[i]} (id, description) VALUES (@id, @description)`);
+    
+    // init a wraper for batch insert
+    let insertMany = db.transaction ( (rows) => {
+        for ( const row of rows ) stmt.run(row);
     });
+    
+    const dataBuffer = [];
 
+    let csv_file_path = path.join( __dirname, "databases", demo_filename[i+1] )
 
-// db.serialize(() => {
-//     console.log('stamp 1');
-//     const stmt = db.prepare(`INSERT INTO ${demo_table_name[0]} VALUES (?, ?)`);
-
-//     // read the csv
-//     console.log('stamp 2');
-//     fs.createReadStream(post_path+demo_filename[3])
-//         .pipe(csv())
-//         .on('data', (row) => {
-//             // row is an object where keys are the record
-//             stmt.run( row.id, 
-//                         row.description);
-//         })
-//         .on('end', () => {
-//                 stmt.finalize();
-//                 console.log(`CSV file for ${demo_filename[3]} has successfully processed`);
-//             });
-//     console.log('stamp 3');
-// }); // end db.serialize()
+    if ( ! fs.existsSync( csv_file_path ) ) {
+        console.log('csv File does not exists');
+        process.exit(-1)
+    }
+    console.log(csv_file_path)
+    
+    fs.createReadStream( csv_file_path )
+        .pipe(csv())
+        .on('data', (row) => {
+            // row is an object where keys are the record
+            if (row.id !== undefined && row.description !== undefined) {
+                dataBuffer.push({
+                    id: row.id, 
+                    description: row.description
+                });
+            }
+        })
+        .on('end', () => {
+            try {
+                console.log(dataBuffer);
+                insertMany(dataBuffer);
+                // console.log(`csv to db completed`)
+                console.log(`CSV file for ${demo_filename[i+1]} has successfully processed. Total rows: ${dataBuffer.length}`);
+            } catch (error) {
+                console.error('Database insertion failed:', error);
+            }
+        });
+} // end i loop
 
 // ==================================================
-
-// // for table: MoneyCurrency
-// db.serialize(() => {
-//     const stmt = db.prepare(`INSERT INTO ${process.argv[2]} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`);
-
-//     // read the csv
-//     fs.createReadStream(process.argv[3])
-//     .pipe(csv())
-//     .on('data', (row) => {
-//         // row is an object where keys are your CSV headers
-//         stmt.run( row.id, 
-//                     row.name, 
-//                     row.account_type, 
-//                     row.description, 
-//                     row.opening_balance, 
-//                     row.currency, 
-//                     row.money_category, 
-//                     row.created_at, 
-//                     row.updated_at );
-//     })
-//     .on('end', () => {
-//             stmt.finalize();
-//             console.log(`CSV file for ${process.argv[3]} has successfully processed`);
-//         });
-// }); // end db.serialize()
+// reminaing table: Accounts, AccountBalance, TransactionJournal, TransactionLedger
 
 
-// // for table: Accounts
-// db.serialize(() => {
-//     const stmt = db.prepare(`INSERT INTO ${process.argv[2]} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`);
-
-//     // read the csv
-//     fs.createReadStream(process.argv[3])
-//     .pipe(csv())
-//     .on('data', (row) => {
-//         // row is an object where keys are your CSV headers
-//         stmt.run( row.id, 
-//                   row.name, 
-//                   row.account_type, 
-//                   row.description, 
-//                   row.opening_balance, 
-//                   row.currency, 
-//                   row.money_category, 
-//                   row.created_at, 
-//                   row.updated_at );
-//     })
-//     .on('end', () => {
-//         stmt.finalize();
-//         console.log(`CSV file for ${process.argv[3]} has successfully processed`);
-//     });
-// }); // end db.serialize()
-
-            
 // i guess like memory allocaiton you need manually "delete" it
 // db.close();
