8b153393a8c3ae3733005324d0035b3c22d39b58
encanTado-A <e232code@gmail.com>
2026-06-02 20:46:23 +0800

    Squashed commit of the following:
    commit 17a872ebde6e00f9df299f65e247b2d24923f681
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 20:11:52 2026 +0800
    
        rm front-end folder
    
    commit 29b6bebb134cb9f33277d6fd7292cba9e7f76471
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 20:11:17 2026 +0800
    
        rm front-end element
    
    commit c74ed7fdd410ed984031dadbf3fc655882229d84
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 20:07:52 2026 +0800
    
        feat: rebuild file structure to seprate js by route or functionality
    
    commit ac550b3aa90b2ef14d0dccadeccb09be3be31104
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 19:37:48 2026 +0800
    
        update: ignore multi-media file and src folder
    
    commit ea1360f4661c334a4a545ada4825b97c02d75e80
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 19:35:31 2026 +0800
    
        rename demo-db.js to demo-db.cjs
    
    commit 537f8839f383e453348eff13b0d0f8b1d7242af6
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 19:34:55 2026 +0800
    
        snap: before moving all to seprate file for routing
    
    commit 45b569ac1026bade5116d1421cfd638a0eeed68d
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 13:56:59 2026 +0800
    
        updated package info
    
    commit 4354aceecd34d6111e5c628d1a61a49fde97adaa
    Author: encanTado-A <e232code@gmail.com>
    Date:   Tue Jun 2 12:43:56 2026 +0800
    
        feat: insert currency + error handling
    
    commit 69fd8474002b63240416c955e76a0c84f7546af5
    Author: encanTado-A <e232code@gmail.com>
    Date:   Mon Jun 1 23:29:33 2026 +0800
    
        feat: continue add demo for input form preparation
    
    commit 7f68204130089525dd9d52ef3a6f4761a4c8ed15
    Author: encanTado-A <e232code@gmail.com>
    Date:   Mon Jun 1 17:18:51 2026 +0800
    
        rm .env
    
    commit 174343b5322d14b3c65cb3403a96325db81f5403
    Author: encanTado-A <e232code@gmail.com>
    Date:   Mon Jun 1 17:18:39 2026 +0800
    
        feat: working demo web server with one of the page is for display query result from sqlite
    
    commit 34f9135d435f7bdc2938fac4446cd5178a83a501
    Author: encanTado-A <e232code@gmail.com>
    Date:   Mon Jun 1 14:43:28 2026 +0800
    
        found ways to import csv to db using node (CJS)
    
    commit 794ae038abf6dd2db2375d283ca4ddb34c5d22a6
    Author: encanTado-A <e232code@gmail.com>
    Date:   Thu May 28 01:24:40 2026 +0800
    
        tmp: functions and scripts for later-on importing demo data and check on database and server logic

---
+++ demo-db.cjs
@@ -0,0 +1,60 @@
+// Cjs
+const Database = require('better-sqlite3');
+const fs = require('fs');
+// const env = require('dotenv');
+
+// env.config();
+
+// check db exist
+const post_path = 'F:\\devp\\cccashflow\\databases\\';
+const demo_filename = [
+    `demo.db`,
+    `demo-account-type.csv`,
+    `demo-accounts.csv`,
+    `demo-currency.csv`,
+    `demo-money-category.csv`,
+];
+
+const demo_table_name = [
+    `Currency`,
+    `AccountType`,
+    `MoneyCategory`,
+    `Accounts`,
+    `AccountBalanace`,
+    `TransactionJournal`,
+    `TransactionLedger`,
+];
+
+let db = null;
+const demoConnectSqliteDB = () => {
+    console.log("status: call from demo-db.js");
+    try {
+        // const db_file_path = post_path+demo_filename[0];
+        const db_file_path = process.env.DEMO_DATABASE_PATH;
+        if ( ! fs.existsSync( db_file_path ) ) {
+            console.log('status: database file does not exists');
+            process.exit(-1);
+        }
+        
+        db = new Database(db_file_path, { verbose: console.log, fileMustExist: true});
+        // if ( db && db.open ) {
+        //     console.log('status: db open');
+        // }
+        // const Database = require('better-sqlite3')(db_file_path, { verbose: console.log });
+        
+        // for (let i = 0; i < demo_filename.length; i++) {
+        //     console.log(`${i}th filename:\t\t${ post_path+demo_filename[i] }`)
+        // };
+    }
+    catch (error) {
+        console.error('status: Database connection failed:', error.message);
+        process.exit(-1);
+    }
+    finally {
+        console.log('status: finish demoConnectSqliteDB');
+    }
+    return db;
+};
+
+// export default demoConnectSqliteDB;
+module.exports = demoConnectSqliteDB;
---
+++ server.mjs
@@ -0,0 +1,178 @@
+// necessary library
+// node.js native
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+// external
+import express from 'express';
+import Database from 'better-sqlite3';
+// import csv from 'csv-parser';
+import env from 'dotenv';
+
+// local js
+import demoConnectSqliteDB from './demo-db.cjs';
+import createRouterAdd from './routeradd.mjs';
+import logger from './logger.mjs';
+
+/*
+potential problem
+path format difference on different OSes
+
+*/
+
+// ##################################################
+
+env.config();
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views')); 
+
+// middleware
+app.use(express.json()); // auto parse JSON  and places result object onto res.body
+app.use(express.urlencoded({ extended: true })) // parse data submitted via HTML <form>
+
+
+
+// ##################################################
+
+let db = null;
+try {
+    console.log('DEMO_DATABASE_PATH=', process.env.DEMO_DATABASE_PATH);
+    
+    db = demoConnectSqliteDB(); // <-- call the function to get Database instance
+    if (db && db.open) {
+        console.log('status: db open');
+        db.pragma('journal_mode = WAL'); // suggested from official for performance reasons
+    } else {
+        console.log('status: db is offline');
+    }
+}
+catch (error) {
+    console.error('status: Database connection failed:', error.message);
+    process.exit(-1);
+}
+
+
+// ##################################################
+// database query function
+
+
+// ##################################################
+// web access function
+
+app.get('/', logger, async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // res.render('index.html', { username: 'Andrew' }); 
+});
+
+// --------------------------------------------------
+// /add pages
+
+// app.use( '/add', Router_add );
+const routeradd = createRouterAdd(db);  // wire db into router
+app.use('/add', routeradd);
+
+// --------------------------------------------------
+// api pages
+
+app.get('/api', logger, async (req, res) => {
+    res.json({
+        status: "demo"
+    });
+    // res.send(result);
+});
+
+app.get('/api/demo/currency', logger, async (req, res) => {
+    // demoConnectSqliteDB
+    var stmt = db.prepare(`SELECT * FROM Currency`);
+    var result = stmt.all();
+        
+    console.log(`${result}`);
+    res.json(result);
+    // res.send(result);
+});
+
+app.get('/api/demo/accounttype', logger, async (req, res) => {
+    // demoConnectSqliteDB
+    var stmt = db.prepare(`SELECT * FROM AccountType`);
+    var result = stmt.all();
+        
+    console.log(`${result}`);
+    res.json(result);
+    // res.send(result);
+});
+
+app.get('/api/demo/accounts', logger, async (req, res) => {
+    // demoConnectSqliteDB
+    var stmt = db.prepare(`SELECT * FROM Accounts`);
+    var result = stmt.all();
+        
+    console.log(`${result}`);
+    res.json(result);
+    // res.send(result);
+});
+
+app.get('/api/demo/transactionsledger', logger, async (req, res) => {
+    // demoConnectSqliteDB
+    var stmt = db.prepare(`SELECT * FROM TransactionsLedger`);
+    var result = stmt.all();
+        
+    console.log(`${result}`);
+    res.json(result);
+    // res.send(result);
+});
+
+app.get('/api/demo/transactionsjournal', logger, async (req, res) => {
+    // demoConnectSqliteDB
+    var stmt = db.prepare(`SELECT * FROM TransactionsJournal`);
+    var result = stmt.all();
+        
+    console.log(`${result}`);
+    res.json(result);
+    // res.send(result);
+});
+
+app.get('/api/demo/AccountBalance', logger, async (req, res) => {
+    // demoConnectSqliteDB
+    var stmt = db.prepare(`SELECT * FROM AccountBalance`);
+    var result = stmt.all();
+        
+    console.log(`${result}`);
+    res.json(result);
+    // res.send(result);
+});
+
+// --------------------------------------------------
+
+// web access
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://${process.env.WEB_TEST_IP}:${process.env.PORT}`)
+});
+
+// ##################################################
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received. Shutting down gracefully.');
+  db.close();
+  process.exit(0);
+//   server.close(() => {
+//     console.log('HTTP server closed.');
+//     // Close database connections or other resources here
+//   });
+});
+
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received.');
+  console.log('HTTP server closed.');
+  db.close();
+  console.log('database server closed.');
+  process.exit(0);
+//   server.close(() => {
+//   });
+});
