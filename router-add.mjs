e9355354abc0b19ef9102a09fdff5d3674c8d6b0
encanTado-A <e232code@gmail.com>
2026-06-05 23:21:49 +0800

    feat: view tables page

--- router-add.mjs
+++ router-add.mjs
@@ -4,18 +4,221 @@ import express from 'express';
 // local js
 import logger from './logger.mjs';
 
+// existing path
+// '/': get
+// '/transaction': get, post
+// '/currency': get, post
 export default function createRouter(db, __dirname) {
     const router = express.Router();
 
+    // --------------------------------------------------
+
     router.get('/', logger, async (req, res) => {
-        res.send("home");
+        return res.send("home");
     }); // end get(/)
+
+    // --------------------------------------------------
+    // transaction related
     
     router.get('/transaction', logger, async (req, res) => {
-        res.render('transaction', { title: 'Add Transaction' });
+        return res.render('basic-transaction', { title: 'Add Transaction' });
     }); // end get(/transaction)
-    
+
     router.post('/transaction', logger, async (req, res) => {
+        const data = req.body;
+        console.log("info: Request Body: " + JSON.stringify(req.body, null, 2));
+        const target_description = req.body.description;
+        const target_date = req.body.date;
+        const target_metadata = req.body.metadata;
+        const target_is_deleted = req.body.is_deleted;
+        const target_account_id_from = req.body.from_account_id;
+        const target_account_id_to = req.body.to_account_id;
+        const target_currency_id = req.body.currency_id || "HKD"; // default HKD
+        const target_is_foregin = req.body.is_foregin || 0; // default 0
+        const target_foreign_amount = req.body.foreign_amount || "";
+        const target_exchange_rate = req.body.exchange_rate || "";
+        
+        // checking stage
+        
+        // 1. if target_account_id_from or target_account_id_to not exist in Account table, then return error
+        // a check both at the same time
+        const stmt_check_account = db.prepare('SELECT COUNT(DISTINCT id) AS count FROM Accounts WHERE id IN(?, ?)');
+        // const stmt_check_account = db.prepare('SELECT COUNT(DISTINCT id) AS both_exist FROM Accounts WHERE id IN(?, ?)');
+
+        const result_check_account = stmt_check_account.get(target_account_id_from, target_account_id_to);
+        if (result_check_account.count != 2) {
+            return res.status(422).json({
+                status: "failed",
+                message: "Invalid account IDs provided"
+            });
+        }
+        // // b check one by one
+        // const stmt_check_account = db.prepare('SELECT COUNT(*) AS count FROM Account WHERE id = ?');
+        // const result_check_account_from = stmt_check_account.get(target_account_id_from);
+        // const result_check_account_to = stmt_check_account.get(target_account_id_to);
+        // if ( ! result_check_account_from && ! result_check_account_to) {
+        //     return res.status(422).json({
+        //         status: "failed",
+        //         message: "Invalid account IDs provided"
+        //     });
+        // }
+
+        // 2. if amount is invalid (not a number or negative), then return error
+        const target_amount = req.body.amount;
+        if (isNaN(target_amount) || target_amount < 0) {
+            return res.status(422).json({
+                status: "failed",
+                message: "Invalid amount provided"
+            });
+        }
+
+        // 3. if date is invalid
+        const check_target_date = new Date(target_date);
+        if (isNaN(check_target_date.getTime())) {
+            return res.status(422).json({
+                status: "failed",
+                message: "Invalid date provided"
+            });
+        }
+        
+        // insert stage
+        const stmt1 = db.prepare(`INSERT INTO TransactionsJournal (from_account_id, to_account_id, description, created_at, metadata, is_deleted) 
+                                    VALUES ( @from_account_id, @to_account_id, @description, @created_at, @metadata, @is_deleted)`);
+        const stmt2 = db.prepare(`INSERT INTO TransactionsLedger 
+            (transactions_id , date_of_transaction, account_id, amount, is_credit, currency, is_foregin, foreign_amount, exchange_rate) 
+            VALUES 
+            (@transactions_id , @date_of_transaction, @account_id, @amount, @is_credit, @currency, @is_foregin, @foreign_amount, @exchange_rate)`);
+
+        // ready function for open transaction in TransactionJournal and TransactionLedger
+        const insertTransaction = db.transaction( (transactionData, ledgerData) => {
+            // 1. insert into transaction journal
+            let info1 = stmt1.run({
+                from_account_id: transactionData.from_account_id,
+                to_account_id: transactionData.to_account_id,
+                description: transactionData.description,
+                created_at: transactionData.created_at,
+                metadata: transactionData.metadata,
+                is_deleted: 0
+            }); 
+
+            if ( ! info1 || info1.changes != 1) {
+                throw new Error(`Failed to insert transaction journal record for this item description: ${transactionData.description}`);
+            }
+            
+            console.log(`info1: ${JSON.stringify(info1)}`)
+            const transactions_id = info1.lastInsertRowid;
+            
+            if ( ! transactions_id || transactions_id <= 0 ) {
+                throw new Error(`Failed to retrieve transaction_id after inserting transaction journal record for this item description: ${transactionData.description}`);
+            }
+
+            // insert into transaction ledger
+            let info2 = stmt2.run({
+                transactions_id: transactions_id,
+                date_of_transaction: ledgerData.date_of_transaction,
+                account_id: ledgerData.from_account_id,
+                amount: ledgerData.amount,
+                is_credit: 1,
+                currency: ledgerData.currency,
+                is_foregin: ledgerData.is_foregin,
+                foreign_amount: ledgerData.foreign_amount,
+                exchange_rate: ledgerData.exchange_rate
+            }); 
+            if ( ! info2 || info2.changes != 1) {
+                throw new Error(`Failed to insert transaction Ledger record with these 2 account description: from ${ledgerData.account_id}  to ${ledgerData.account_id} with item description: ${ledgerData.description}`);
+            }
+
+            let info3 = stmt2.run({
+                transactions_id: transactions_id,
+                date_of_transaction: ledgerData.date_of_transaction,
+                account_id: ledgerData.to_account_id,
+                amount: ledgerData.amount,
+                is_credit: 0,
+                currency: ledgerData.currency,
+                is_foregin: ledgerData.is_foregin,
+                foreign_amount: ledgerData.foreign_amount,
+                exchange_rate: ledgerData.exchange_rate
+            });
+
+            // post transaction checking
+            // -> is the amount is 0 at the end?
+
+            if ( ! info3 && ! info3.changes == 1) {
+                throw new Error(`Failed to insert transaction Ledger record with these 2 account description: from ${ledgerData.account_id}  to ${ledgerData.account_id} with item description: ${ledgerData.description}`);
+            }
+
+            return {info1 , info2, info3};
+        }); // end insertTransaction
+            
+        // insert action
+        let info = null;
+        try {
+            let info = insertTransaction({
+                description: target_description, 
+                from_account_id: target_account_id_from,
+                to_account_id: target_account_id_to,
+                created_at: target_date, 
+                metadata: target_metadata, 
+                is_deleted: target_is_deleted
+            }, {
+                date_of_transaction: target_date,
+                from_account_id: target_account_id_from,
+                to_account_id: target_account_id_to,
+                amount: target_amount,
+                currency: target_currency_id,
+                is_foregin: target_is_foregin,
+                foreign_amount: target_foreign_amount,
+                exchange_rate: target_exchange_rate
+            });
+
+        }
+        catch (SqliteError) {
+            return res.status(422).json({ 
+                status: "failed", 
+                message: `insertion failed`, 
+                details: `SQLite Error: ${SqliteError.message}`,
+                sqlaction: `ROLLBACK`, 
+            });
+        }
+
+        // MYSQL / MARIADB
+        // const check_stmt_result = db.prepare(`SELECT sum(IF is_credit, amount, 0) AS total_debit, 
+        //                             sum(IF NOT(is_credit), amount, 0) AS total_credit 
+        //                             FROM transactionsJournal`).run();
+        // sqlite
+        const check_stmt_result = db.prepare(`SELECT sum(CASE WHEN is_credit THEN amount ELSE 0 END) AS total_debit, 
+                                    sum(CASE WHEN NOT is_credit THEN amount ELSE 0 END) AS total_credit 
+                                    FROM transactionsJournal`).get();
+        if ( check_stmt_result.total_debit - check_stmt_result.total_credit != 0 ) {
+            return res.status(500).json({ 
+                status: "failed", 
+                message: `transaction record not accept`, 
+                detail: `internal error` 
+            });
+        }
+
+        // if ( ! info || info.changes != 1 ) {
+        //     return res.status(422).json({ 
+        //         status: "failed", 
+        //         message: `transaction record not accept`, 
+        //     });
+        // }
+        
+        if (info && info.info1 && info.info1.lastInsertRowid) {
+            console.log(`info: ${JSON.stringify(info)}`)
+            // console.log(`Transaction inserted successfully with transaction_id: ${info[0].lastInsertRowid}`);
+        }
+        // const transaction_id = info[0].lastInsertRowid;
+        return res.redirect('/add/transaction')
+
+    }); // end post(/transaction)
+
+
+    router.get('/v1/transaction', logger, async (req, res) => {
+        return res.render('transaction', { title: 'Add Transaction' });
+    }); // end get(/v1/transaction)
+    
+    router.post('/v1/transaction', logger, async (req, res) => {
         const data = req.body;
         console.log("info: Request Body: " + JSON.stringify(req.body, null, 2));
     
@@ -36,7 +239,7 @@ export default function createRouter(db, __dirname) {
             });
         }
         catch (SqliteError) {
-            res.status(422).json({ 
+            return res.status(422).json({ 
                 status: "failed", 
                 message: `data received but invalid currency!  currency: ${target_id}  description: ${target_description}`, 
                 details: `SQLite Error: ${SqliteError.message}`
@@ -44,20 +247,20 @@ export default function createRouter(db, __dirname) {
         }
     
         if ( info && info.changes != 1 ) {
-            res.status(422).json({ 
+            return res.status(422).json({ 
                 status: "failed", 
                 message: `data received but invalid currency!  currency: ${target_id}  description: ${target_description}` 
             });
         }
         
-        res.status(200).json({ 
+        return res.status(200).json({ 
             status: "success", 
             message: `data well received!  new currency: ${target_id}  description: ${target_description}` 
         });
-    }); // end post(/transaction)
+    }); // end post(/v1/transaction)
     
     router.get('/demo/transaction', logger, async (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'input-form.html'));
+        return res.sendFile(path.join(__dirname, 'public', 'input-form.html'));
     }); // end get(/demo/transaction)
     
     router.post('/demo/transaction', logger, async (req, res) => {
@@ -65,12 +268,14 @@ export default function createRouter(db, __dirname) {
         console.log(`Received data: \n${data}`);
         console.log("Request Body: " + JSON.stringify(req.body, null, 2));
         
-        res.status(200).json({ 
+        return res.status(200).json({ 
             status: "success", 
             message: `data well received!` 
         });
     }); // end post(/demo/transaction)
     
+    // --------------------------------------------------
+
     router.get('/currency', logger, async (req, res) => {
         res.sendFile(path.join(__dirname, 'public', 'new-currency.html'));
     }); // end get(/currency)
@@ -90,7 +295,7 @@ export default function createRouter(db, __dirname) {
             info = stmt.run({id: target_id, description: target_description});
         }
         catch (SqliteError) {
-            res.status(422).json({ 
+            return res.status(422).json({ 
                 status: "failed", 
                 message: `data received but invalid currency!  currency: ${target_id}  description: ${target_description}`, 
                 details: `SQLite Error: ${SqliteError.message}`
@@ -98,14 +303,14 @@ export default function createRouter(db, __dirname) {
         }
     
         if ( info && info.changes != 1 ) {
-            res.status(422).json({ 
+            return res.status(422).json({ 
                 status: "failed", 
                 message: `data received but invalid currency!  currency: ${target_id}  description: ${target_description}` 
             });
         }
         
         // Send a JSON response back to the frontend
-        res.status(200).json({ 
+        return res.status(200).json({ 
             status: "success", 
             message: `data well received!  new currency: ${target_id}  description: ${target_description}` 
         });
--- router-view.mjs
+++ router-view.mjs
@@ -8,20 +8,41 @@ export default function createRouter(db, __dirname) {
     const router = express.Router();
 
     router.get('/tables', logger, async (req, res) => {
-        res.render('default-query-display', { title: 'view home' });
-    }); // end get(/)
+        // ai
+        const queryTableName = req.query.table_name; 
+        console.log(`Query data received: ${queryTableName}`);
+        res.render('default-query-display', { 
+            title: 'view tables', 
+            tableSelect: queryTableName || null
+        });
+        // console.log(`/tables :: req.body: ${req.query}`);
+        // res.render('default-query-display', { title: 'view tables' });
+    }); // end get(/tables)
+
+    // router.get('/tables/:table_name', logger, async (req, res) => {
+    //     console.log(`/tables:table_name :: req.body: ${req.query}`);
+    //     // const tmp = table_name;
+    //     const the_table_name = req.params.table_name;
+    //     console.log(`the_table_name: ${the_table_name}`);
+    //     res.render('default-query-display', { title: 'view tables', tableSelect: the_table_name });
+    // }); // end get(/tables/:table_name)
 
     router.get('/dashboard', logger, async (req, res) => {
         res.status(200).render("dashboard", { title: 'Dashboard' });
     }); // end get(/)
 
     router.get('/currency', logger, async (req, res) => {
-        res.status(302).redirect('/view/tables');
+        res.redirect('/view/tables');
     }); // end get(/currency)
     
     router.get('/transaction', logger, async (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'input-form.html'));
+        res.redirect('/view/tables');
     }); // end get(/transaction)
+
+    router.get('/transaction/delete_record', logger, async (req, res) => {
+        // TBC
+        res.redirect('/view/tables');
+    }); // end get(/currency)
     
     return router;
 }; // end createRouter()
