376dbf5cf544ebd99f732c7e6589a175bce4159c
encanTado-A <e232code@gmail.com>
2026-06-24 23:57:15 +0800

    add: redirect routing, use updated transaction.ejs to render, update post /add/transaction

--- router-add.mjs
+++ router-add.mjs
@@ -14,14 +14,31 @@ export default function createRouter(db, __dirname) {
     // --------------------------------------------------
 
     router.get('/', logger, async (req, res) => {
-        return res.send("home");
+        const targetTable = req.query.type ?? 0;
+        if ( !(req.query && targetTable) ) {
+            return res.send("home");
+        }
+        switch (targetTable) {
+            case "transaction":
+                return res.redirect("/add/transaction");
+                break;
+            case "account":
+                return res.redirect("/add/account");
+                break;
+            case "category":
+                return res.redirect("/add/category");
+                break;
+            case "currency":
+                return res.redirect("/add/currency");
+                break;
+        }
     }); // end get(/)
 
     // --------------------------------------------------
     // transaction related
     
     router.get('/transaction', logger, async (req, res) => {
-        return res.render('basic-transaction', { title: 'Add Transaction' });
+        return res.render('transaction', { title: 'Add Transaction' });
     }); // end get(/transaction)
 
     router.post('/transaction', logger, async (req, res) => {
@@ -31,37 +48,44 @@ export default function createRouter(db, __dirname) {
         const target_date = req.body.date;
         const target_metadata = req.body.metadata;
         const target_is_deleted = req.body.is_deleted;
-        const target_account_id_from = req.body.from_account_id;
-        const target_account_id_to = req.body.to_account_id;
+        const target_account_id_from = req.body.account_id_from;
+        const target_account_id_to = req.body.account_id_to;
         const target_currency_id = req.body.currency_id || "HKD"; // default HKD
-        const target_is_foregin = req.body.is_foregin || 0; // default 0
+        const target_is_foreign = req.body.is_foreign || 0; // default 0
         const target_foreign_amount = req.body.foreign_amount || "";
         const target_exchange_rate = req.body.exchange_rate || "";
-        
+
+        // --------------------------------------------------
         // checking stage
         
-        // 1. if target_account_id_from or target_account_id_to not exist in Account table, then return error
-        // a check both at the same time
-        const stmt_check_account = db.prepare('SELECT COUNT(DISTINCT id) AS count FROM Accounts WHERE id IN(?, ?)');
-        // const stmt_check_account = db.prepare('SELECT COUNT(DISTINCT id) AS both_exist FROM Accounts WHERE id IN(?, ?)');
+        // // 1. if target_account_id_from or target_account_id_to not exist in Account table, then return error
+        // // a check both at the same time
+        // const stmt_check_account = db.prepare('SELECT COUNT(DISTINCT id) AS count FROM Accounts WHERE id IN(?, ?)');
+        // // const stmt_check_account = db.prepare('SELECT COUNT(DISTINCT id) AS both_exist FROM Accounts WHERE id IN(?, ?)');
 
-        const result_check_account = stmt_check_account.get(target_account_id_from, target_account_id_to);
-        if (result_check_account.count != 2) {
-            return res.status(422).json({
-                status: "failed",
-                message: "Invalid account IDs provided"
-            });
-        }
-        // // b check one by one
-        // const stmt_check_account = db.prepare('SELECT COUNT(*) AS count FROM Account WHERE id = ?');
-        // const result_check_account_from = stmt_check_account.get(target_account_id_from);
-        // const result_check_account_to = stmt_check_account.get(target_account_id_to);
-        // if ( ! result_check_account_from && ! result_check_account_to) {
+        // const result_check_account = stmt_check_account.get(target_account_id_from, target_account_id_to);
+        // if (result_check_account.count != 2) {
         //     return res.status(422).json({
         //         status: "failed",
         //         message: "Invalid account IDs provided"
         //     });
         // }
+        // b check one by one
+        if ( !(target_account_id_from || target_account_id_to)) {
+            return res.status(422).json({
+                status: "failed",
+                message: "Invalid account IDs provided"
+            });
+        }
+        const stmt_check_account = db.prepare('SELECT COUNT(*) AS count FROM Accounts WHERE id = ?');
+        const result_check_account_from = stmt_check_account.get(target_account_id_from);
+        const result_check_account_to = stmt_check_account.get(target_account_id_to);
+        if ( ! result_check_account_from && ! result_check_account_to) {
+            return res.status(422).json({
+                status: "failed",
+                message: "Invalid account IDs provided"
+            });
+        }
 
         // 2. if amount is invalid (not a number or negative), then return error
         const target_amount = req.body.amount;
@@ -82,12 +106,12 @@ export default function createRouter(db, __dirname) {
         }
         
         // insert stage
-        const stmt1 = db.prepare(`INSERT INTO TransactionsJournal (from_account_id, to_account_id, description, created_at, metadata, is_deleted) 
-                                    VALUES ( @from_account_id, @to_account_id, @description, @created_at, @metadata, @is_deleted)`);
+        const stmt1 = db.prepare(`INSERT INTO TransactionsJournal (description, created_at, metadata, is_deleted) 
+                                    VALUES ( @description, @created_at, @metadata, @is_deleted)`);
         const stmt2 = db.prepare(`INSERT INTO TransactionsLedger 
-            (transactions_id , date_of_transaction, account_id, amount, is_credit, currency, is_foregin, foreign_amount, exchange_rate) 
+            (transaction_id , date_of_transaction, account_id, amount, is_credit, currency, is_foreign, exchange_rate) 
             VALUES 
-            (@transactions_id , @date_of_transaction, @account_id, @amount, @is_credit, @currency, @is_foregin, @foreign_amount, @exchange_rate)`);
+            (@transaction_id , @date_of_transaction, @account_id, @amount, @is_credit, @currency, @is_foreign, @exchange_rate)`);
 
         // ready function for open transaction in TransactionJournal and TransactionLedger
         const insertTransaction = db.transaction( (transactionData, ledgerData) => {
@@ -106,21 +130,21 @@ export default function createRouter(db, __dirname) {
             }
             
             console.log(`info1: ${JSON.stringify(info1)}`)
-            const transactions_id = info1.lastInsertRowid;
+            const transaction_id = info1.lastInsertRowid;
             
-            if ( ! transactions_id || transactions_id <= 0 ) {
+            if ( ! transaction_id || transaction_id <= 0 ) {
                 throw new Error(`Failed to retrieve transaction_id after inserting transaction journal record for this item description: ${transactionData.description}`);
             }
 
             // insert into transaction ledger
             let info2 = stmt2.run({
-                transactions_id: transactions_id,
+                transaction_id: transaction_id,
                 date_of_transaction: ledgerData.date_of_transaction,
                 account_id: ledgerData.from_account_id,
                 amount: ledgerData.amount,
                 is_credit: 1,
                 currency: ledgerData.currency,
-                is_foregin: ledgerData.is_foregin,
+                is_foreign: ledgerData.is_foreign,
                 foreign_amount: ledgerData.foreign_amount,
                 exchange_rate: ledgerData.exchange_rate
             }); 
@@ -129,13 +153,13 @@ export default function createRouter(db, __dirname) {
             }
 
             let info3 = stmt2.run({
-                transactions_id: transactions_id,
+                transaction_id: transaction_id,
                 date_of_transaction: ledgerData.date_of_transaction,
                 account_id: ledgerData.to_account_id,
                 amount: ledgerData.amount,
                 is_credit: 0,
                 currency: ledgerData.currency,
-                is_foregin: ledgerData.is_foregin,
+                is_foreign: ledgerData.is_foreign,
                 foreign_amount: ledgerData.foreign_amount,
                 exchange_rate: ledgerData.exchange_rate
             });
@@ -145,6 +169,25 @@ export default function createRouter(db, __dirname) {
 
             if ( ! info3 && ! info3.changes == 1) {
                 throw new Error(`Failed to insert transaction Ledger record with these 2 account description: from ${ledgerData.account_id}  to ${ledgerData.account_id} with item description: ${ledgerData.description}`);
+            }
+
+            // check sum
+            // MYSQL / MARIADB
+            // const check_stmt_result = db.prepare(`SELECT sum(IF is_credit, amount, 0) AS total_debit, 
+            //                             sum(IF NOT(is_credit), amount, 0) AS total_credit 
+            //                             FROM transactionsJournal`).run();
+            // sqlite
+            const check_stmt_result = db.prepare(`SELECT sum(CASE WHEN is_credit THEN 0 ELSE amount END) AS total_debit, 
+                                        sum(CASE WHEN is_credit THEN amount ELSE 0 END) AS total_credit 
+                                        FROM transactionsLedger`).get();
+            console.log(JSON.stringify(check_stmt_result, null, 2));
+            if ( check_stmt_result.total_debit - check_stmt_result.total_credit != 0 ) {
+                // return res.status(500).json({ 
+                //     status: "failed", 
+                //     message: `transaction record not accept`, 
+                //     detail: `internal error` 
+                // });
+                throw new Error(`Error: Failed to insert transaction Ledger record with these 2 account description: from ${target_account_id_from}  to ${target_account_id_to} with item description: ${target_description}. Check Sum.`);
             }
 
             return {info1 , info2, info3};
@@ -166,27 +209,10 @@ export default function createRouter(db, __dirname) {
                 to_account_id: target_account_id_to,
                 amount: target_amount,
                 currency: target_currency_id,
-                is_foregin: target_is_foregin,
+                is_foreign: target_is_foreign,
                 foreign_amount: target_foreign_amount,
                 exchange_rate: target_exchange_rate
             });
-
-            // MYSQL / MARIADB
-            // const check_stmt_result = db.prepare(`SELECT sum(IF is_credit, amount, 0) AS total_debit, 
-            //                             sum(IF NOT(is_credit), amount, 0) AS total_credit 
-            //                             FROM transactionsJournal`).run();
-            // sqlite
-            const check_stmt_result = db.prepare(`SELECT sum(CASE WHEN is_credit THEN amount ELSE 0 END) AS total_debit, 
-                                        sum(CASE WHEN NOT is_credit THEN amount ELSE 0 END) AS total_credit 
-                                        FROM transactionsLedger`).get();
-            if ( check_stmt_result.total_debit - check_stmt_result.total_credit != 0 ) {
-                // return res.status(500).json({ 
-                //     status: "failed", 
-                //     message: `transaction record not accept`, 
-                //     detail: `internal error` 
-                // });
-                throw new Error(`Error: Failed to insert transaction Ledger record with these 2 account description: from ${ledgerData.account_id}  to ${ledgerData.account_id} with item description: ${ledgerData.description}. Check Sum.`);
-            }
         }
         catch (SqliteError) {
             return res.status(422).json({ 
@@ -216,7 +242,7 @@ export default function createRouter(db, __dirname) {
 
 
     router.get('/v1/transaction', logger, async (req, res) => {
-        return res.render('transaction', { title: 'Add Transaction' });
+        return res.render('basic-transaction', { title: 'Add Transaction' });
     }); // end get(/v1/transaction)
     
     router.post('/v1/transaction', logger, async (req, res) => {
