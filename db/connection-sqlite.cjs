e383765aaf134e62f6861dd475b4320975474fdd
encanTado-A <e232code@gmail.com>
2026-07-22 23:22:41 +0800

    add: sqlite connection un-test

---
+++ db/connection-sqlite.cjs
@@ -0,0 +1,41 @@
+// Cjs
+const Database = require('better-sqlite3');
+const fs = require('fs');
+const path = require('path');
+// const env = require('dotenv');
+
+// env.config();
+
+// ##################################################
+
+let db = null;
+
+const connectSqliteDB = (verbose) => {
+    console.log("status: call from test-db-connect.cjs");
+    console.log(`status: connecting to ${process.env.TEST_DATABASE_FILENAME}`);
+    try {
+        const db_file_path = path.join(process.env.TEST_DATABASE_PATH, process.env.TEST_DATABASE_FILENAME);
+
+        if ( ! fs.existsSync( db_file_path ) ) {
+            console.log('status: database file does not exists');
+            process.exit(-1);
+        }
+        
+        if ( verbose ) {
+            db = new Database(db_file_path, { verbose: console.log, fileMustExist: true});
+        }
+        else {
+            db = new Database(db_file_path, { fileMustExist: true});
+        }
+    }
+    catch (error) {
+        console.error('status: Database connection failed:', error.message);
+        process.exit(-1);
+    }
+    finally {
+        console.log('status: finish demoConnectSqliteDB');
+    }
+    return db;
+}; // end connectSqliteDB()
+
+module.exports = connectSqliteDB;
