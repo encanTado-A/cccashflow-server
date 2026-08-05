1ee18efa08b79dc213e8744214f65c813e33c66b
encanTado-A <e232code@gmail.com>
2026-08-05 20:00:54 +0800

    fix: update query table name

--- db/utili-sqlite.mjs
+++ db/utili-sqlite.mjs
@@ -1,23 +1,23 @@
 export default function utili_sqlite_function(db) {
     function getUserForLoginByUsername(username) {
-        const stmt = db.prepare( 'SELECT * FROM User WHERE username = ?' );
+        const stmt = db.prepare( 'SELECT * FROM Users WHERE username = ?' );
         const result = stmt.get( username );
         return result;
     };
 
     function getUserByUsername(username) {
-        const stmt = db.prepare( 'SELECT count(id) FROM User WHERE username = ?' );
+        const stmt = db.prepare( 'SELECT count(id) FROM Users WHERE username = ?' );
         const result = stmt.get( username );
         return result;
     };
 
     function getUserById(id) {
-        const stmt_result = db.prepare(`SELECT * from User WHERE id = ?`).get( id );
+        const stmt_result = db.prepare(`SELECT * from Users WHERE id = ?`).get( id );
         return stmt_result;
     };
     
     function getHashPasswordByUsername(username) {
-        const stmt = db.prepare( 'SELECT password FROM User WHERE username = ?' );
+        const stmt = db.prepare( 'SELECT password FROM Users WHERE username = ?' );
         const result = stmt.get( username );
         return result;
     };
